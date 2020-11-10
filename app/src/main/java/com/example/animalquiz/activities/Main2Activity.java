9b9adcd4d9ffe3a1481c10058e75a3ddde0c5cda
Juan Carlos <juancarlospedraza.1990@gmail.com>
2020-11-10 12:53:16 +0100

    mapa

--- app/src/main/java/com/example/animalquiz/activities/Main2Activity.java
+++ app/src/main/java/com/example/animalquiz/activities/Main2Activity.java
@@ -2,15 +2,35 @@ package com.example.animalquiz.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.animalquiz.MapsActivityMapa;
 import com.example.animalquiz.R;
 
 public class Main2Activity extends AppCompatActivity {
 
+    Button btnMapa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        btnMapa = (Button) findViewById(R.id.btnMapa);
+
+        btnMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivityMapa.class);
+
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
